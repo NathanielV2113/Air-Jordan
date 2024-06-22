625d87d53cdbe375ebdfc920944b435089e77c80
Nathaniel <nathanielvasquez329@gmail.com>
2024-06-22 13:39:12 +0800

    login done

--- script.js
+++ script.js
@@ -19,13 +19,38 @@
     }
 
     window.onload = init;
-  
+
     function init(){
         var bar = document.getElementById('bar');
         bar.onclick = function(){
-            var nav = document.querySelector('nav');
-            nav.style.display = 'flex';
+            const nav = document.querySelector(".nav");
+            nav.classList.toggle('active');
         }
+
+        var login = document.getElementById('login_btn');
+        login.onclick = function(){
+            window.location.replace("./login.html");
+        }
+
+        const log_btn = document.querySelector("#log-btn");
+        const reg_btn = document.querySelector("#reg-btn");
+        const log_btn2 = document.querySelector("#log2");
+        const reg_btn2 = document.querySelector("#reg2");
+        const container = document.querySelector(".login-reg");
+
+        reg_btn.addEventListener("click", () =>{
+            container.classList.add("register-mode");
+        })
+        log_btn.addEventListener("click", () =>{
+            container.classList.remove("register-mode");
+        })
+
+        reg_btn2.addEventListener("click", () =>{
+            container.classList.add("register-mode2");
+        })
+        log_btn2.addEventListener("click", () =>{
+            container.classList.remove("register-mode2");
+        })
     }
   
   })(window, document, undefined);
